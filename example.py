786da0bb86873b1322b0215af02cf2d48b21a5ed
sogangCSmaster <cs20131575@u.sogang.ac.kr>
2021-04-21 18:17:48 +0900

    searchUtil added

---
+++ example.py
@@ -0,0 +1,40 @@
+import searchUtil
+# 네이버 서치 유틸
+
+# ----------------------------------
+# keyword: 검색 키워드
+# advertiser: 광고주 URL substring
+# ----------------------------------
+
+keyword = "콜라겐"
+advertiser = "newtreemall"
+
+# init class
+collagenKeyword = searchUtil.NaverSearch(keyword, advertiser)
+print(collagenKeyword)
+# 광고주: newtreemall, 키워드: 콜라겐
+
+
+# currentPCTotalPowerLink Method
+# 현재 해당 키워드로 PC에 첫페이지에 뜨는 총 파워링크 개수
+totalAds = collagenKeyword.currentPCTotalPowerLink()
+print(totalAds)
+
+
+# currentPCRank Method
+# 현재 PC 랭크 순위
+# return 값이 -1일 경우 첫페이지에 없는 상태
+rank = collagenKeyword.currentPCRank()
+print(rank)
+
+
+# currentMoTotalPowerLink Method
+# 현재 해당 키워드로 보이는 모바일 파워링크 첫페이지에 뜨는 모든 광고
+totalMobileAds = collagenKeyword.currentMOTotalPowerLink()
+print(totalMobileAds)
+
+# currentMORank Method
+# 현재 MO 랭크 순위
+# return 값이 -1일 경우 첫페이지에 없는 상태
+mobileRank = collagenKeyword.currentMORank()
+print(mobileRank)
---
+++ searchUtil.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+
+class NaverSearch:
+    def __init__(self, keyword, advertiser):
+        self.keyword = keyword
+        self.advertiser = advertiser
+
+    def __str__(self):
+        return '광고주: {}, 키워드: {}'.format(self.advertiser, self.keyword)
+    
+    def currentPCTotalPowerLink(self):
+        ENDPOINT = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + self.keyword
+        page = requests.get(ENDPOINT)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        power_link_body = soup.find('div', {'id': 'power_link_body'})
+        AS = power_link_body.find_all('a', {'class': 'lnk_url'})
+        return len(AS)
+
+    def currentPCRank(self):
+        ENDPOINT = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + self.keyword
+        page = requests.get(ENDPOINT)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        power_link_body = soup.find('div', {'id': 'power_link_body'})
+
+        # ul = power_link_body.find('ul', {'class': 'lst_type'})
+        AS = power_link_body.find_all('a', {'class': 'lnk_url'})
+        rank = 0
+        for index, a in enumerate(AS):
+            if(self.advertiser in a.get_text()):
+                return index + 1
+        return -1
+
+    def currentMOTotalPowerLink(self):
+        ENDPOINT = 'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=' + self.keyword
+        page = requests.get(ENDPOINT)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        power_link_body = soup.find('ul', {'id': 'power_link_body'})
+        AS = power_link_body.find_all('span', {'class': 'url'})
+        return len(AS)
+        rank = 0
+        for index, a in enumerate(AS):
+            if(self.advertiser in a.get_text()):
+                return {'rank': index + 1, 'totalAds': len(AS)}
+
+        return {'rank': rank, 'totalAds': len(AS)}
+
+    def currentMORank(self):
+        ENDPOINT = 'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=' + self.keyword
+        page = requests.get(ENDPOINT)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        power_link_body = soup.find('ul', {'id': 'power_link_body'})
+        AS = power_link_body.find_all('span', {'class': 'url'})
+        for index, a in enumerate(AS):
+            if(self.advertiser in a.get_text()):
+                return index+1
+        return -1
